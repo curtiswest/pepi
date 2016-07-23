24f925a854dda49279eb7316ba55299793ce5ce8
Claudio Pizzolato <claudio.pizzolato@griffithuni.edu.au>
2016-07-23 02:28:11 +0000

    Add camera preview and change picture resolution

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import numpy as np
 from picamera import PiCamera
 import sys
 import signal
+import io
 
 run_condition = True
 
@@ -16,16 +17,23 @@ def signal_handler(signal, frame):
         sys.exit(0)
 
 def generateRandomImg():
-    z = np.random.random((500, 500))  # Test data
+    z = np.random.random((2592, 1944))  # Test data
     print z.dtype
     return z
 
 def getCameraStill():
+    stream = io.BytesIO()
     with PiCamera() as camera:
-        camera.resolution=(500,500)
-        camera.capture('temp.bmp')
-    data = np.asarray(cv2.imread('temp.bmp'), dtype='uint16')
-    return data
+        camera.resolution=(2592, 1944)
+	camera.start_preview()
+	time.sleep(5)
+        camera.capture(stream, "bmp")
+	camera.stop_preview()
+    data = np.fromstring(stream.getvalue(), dtype='uint8')
+    image = cv2.imdecode(data, 1)
+#    data = np.asarray(cv2.imread('temp.bmp'), dtype='uint16')
+    data_to_send = np.asarray(image, dtype='uint16')
+    return data_to_send
 
 def getData():
     z = getCameraStill()
@@ -40,25 +48,28 @@ def waitForClient(sock):
     print "received ", msg
     return connection
 
-camera_id = sys.argv[1].zfill(2)
-signal.signal(signal.SIGINT, signal_handler)
-print 'starting server ', camera_id
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("", 10006))
-server_socket.listen(5)
+def __init__():
+    with open(sys.argv[1]) as f:
+        camera_id = f.readline().zfill(2)
+    signal.signal(signal.SIGINT, signal_handler)
+    print 'starting server ', camera_id
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("", 10006))
+    server_socket.listen(5)
 
-while(run_condition):
-    try:
-        connection = waitForClient(server_socket)
-        print "sending ", camera_id
-        communication.send_msg(connection, camera_id)
-        print "received ", communication.recv_msg(connection)
-        data = getData()
-        print "sending image data"
-        communication.send_img(connection, data)
-        print "closing connection"
-        connection.close()
-    except:
-        print "Server failure, resetting connection"
-server_socket.close()
+    while(run_condition):
+        try:
+            connection = waitForClient(server_socket)
+            print "sending ", camera_id
+            communication.send_msg(connection, camera_id)
+            print "received ", communication.recv_msg(connection)
+            data = getData()
+            print "sending image data"
+            communication.send_img(connection, data)
+            print "closing connection"
+            connection.close()
+        except:
+            print "Server failure, resetting connection"
+    server_socket.close()
 
+__init__()
