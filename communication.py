63bfd808e41f70c979e899ef78ba79d1e35cb09c
Curtis West <curtis@curtiswest.net>
2017-07-07 18:06:22 +1000

    Add sending strings over the send_command function.
    Add documentation to modified functions.
    Add more robust error checking with exceptions thrown as appropriate.

--- communication.py
+++ communication.py
@@ -28,6 +28,7 @@ def wait_for_msg(sock):
 
     Returns:
         the length of the message incoming message in bytes
+
     Raises:
         SocketError: when connection is lost/closed on the socket while waiting for a message
     """
@@ -39,6 +40,29 @@ def wait_for_msg(sock):
         raise
 
 
+def generate_format_string(values_to_pack):
+    """
+    Generates a format string used for packing strings, ints and long into strings with the `struct` module.
+
+    Args:
+        values_to_pack: a list containing ints, long or strings to generate the format string for
+
+    Returns:
+        str: the format string
+    """
+    format_string = '>'
+    if not isinstance(values_to_pack, (list,tuple)):
+        values_to_pack = [values_to_pack]
+
+    for value in values_to_pack:
+        if isinstance(value, str):
+            format_string += (str(len(value)) + 's')
+        elif isinstance(value, (int,long)):
+            format_string += 'L'
+        else:
+            raise ValueError('Format strings can only be generated for int, long and string types.')
+    return format_string
+
 def send_command(sock, command, values=None):
     """
     Sends a command as a binary-packed string over the given socket with the given optional values.
@@ -46,41 +70,91 @@ def send_command(sock, command, values=None):
     Args:
         sock (socket): the socket over which the command will be sent
         command (int): a one-dimension list or tuple of int values associated with the command.
-        values ([int]): the values to send with the `command`. `Values` must be a single-dimension list or tuple
-            with each element of type int. Defaults to None.
+        values ([int or long or str]): the values to send along with the `command`. `Values` must be a single-dimension
+            list or tuple with each element being one of the following types: int, long, string. Defaults to None.
 
     Returns:
         None: on success
+
     Raises:
-        ValueError: when at least one of the elements of `values` is not an `int` (including when a multi-dimensional
-            `values` are passed).
-        socket.error: when an error occurs during transmission over the socket. An unspecified amount of data may have
+        ValueError: when at least one of the elements of `values` is not of type int, long, string or when it is
+            multi-dimensional (e.g. a list) and therefore cannot be packed.
+        SocketError: when an error occurs during transmission over the socket. An unspecified amount of data may have
             been sent over the socket before the error is raised.
     """
-    fmt_string = '>L' + 'L'*len(values)  # Command, and the actual values tuples
+    def _check_types_of(_values):
+        if not all(isinstance(x, (int, long, str)) for x in _values):
+            raise ValueError('{} contains an element that is a not of type int, long, or string '.format(_values) +
+                             'or alternatively is a list or tuple. The list of values must take that format.')
+
     values_to_pack = [command]
-    values_to_pack.extend(values)
-    for value in values_to_pack:
-        # Ensure values_to_pack only contains ints before packing
-        if not isinstance(value, (int, long)):
-            raise ValueError('A provided value ({} from {}) is not convertible to an int.'.format(value, values))
-    packed_data = struct.pack(fmt_string, *values_to_pack)
+    if values is not None:
+        if not isinstance(values, (list, tuple)):
+            values = [values] # Must be in a list before checking and packing
+        _check_types_of(values)
+        values_to_pack.extend(values)
+
     try:
+        # The packed data being sent to the send_msg function is a binary-packed string of the format:
+        #   fmt_string_len : 4 bytes representing length (z bytes) of the format string to unpack
+        #   fmt_string: z bytes containing the format string to unpack the rest of the message
+        #   remaining message: remaining bytes up to msg_len, unpacked with the format_string where:
+        #       cmd: the first element in the unpacked message i.e. [0]
+        #       values: the remaining elements in the unpacked message i.e. [1:]
+        fmt_string = generate_format_string(values_to_pack)
+        values_to_pack = [fmt_string] + values_to_pack
+        values_to_pack = [len(fmt_string)] + values_to_pack
+        fmt_string = generate_format_string(values_to_pack)
+        packed_data = struct.pack(fmt_string, *values_to_pack)
         send_msg(sock, packed_data)
     except:
         raise
 
 def recv_command(sock, msg_len=None):
+    """
+    Receives a command and any associated values over the `socket`.
+
+    Args:
+        sock: the socket over which the command will be received
+        msg_len: the length of the message to receive in bytes. Defaults to None, in which case the function will call
+            wait_for_msg on this socket to retrieve a message length (which will make this function block).
+
+    Returns:
+        (cmd (int), values ([])): a tuple where the first element is the received command, and the second is
+            a list of values (of type int, long, or string) received, if any.
+
+    Raises:
+        SocketError: when an error occurs while receiving the packed message
+        struct.error: when an error occurs during unpacked the message, presumably because it has been encoding incorrectly
+            or corrupted at some stage.
+    """
+    # If not given a message length, then wait for an incoming message
     if msg_len is None:
         msg_len = wait_for_msg(sock)
 
-    # Now, retrieve the message's command and values tuple
+    # Now, retrieve remaining message of length msg_len
     packed_msg = recvall(sock, msg_len)
     if packed_msg is None:
         raise SocketError('Reached end of socket ')
-    unpacked_msg = struct.unpack('>'+'L'*(msg_len/4), packed_msg)
-    cmd = unpacked_msg[0]
-    values = unpacked_msg[1:]
+
+    try:
+        # Recall that a command message is a binary-packed string of the format:
+        #   msg_len : 4 bytes
+        #   fmt_string_len : 4 bytes representing length (z bytes) of the format string to unpack
+        #   fmt_string: z bytes containing the format string to unpack the rest of the message
+        #   remaining message: remaining bytes up to msg_len, unpacked with the format_string where:
+        #       cmd: the first element in the unpacked message i.e. [0]
+        #       values: the remaining elements in the unpacked message i.e. [1:]
+        # As such, it is necessary to step through the received message and pull out each of the above in order.
+        fmt_string_len = struct.unpack('>L', packed_msg[0:4])[0]
+        packed_msg = packed_msg[4:]  # Move forward
+        fmt_string = struct.unpack('>' + str(fmt_string_len) + 's', packed_msg[:fmt_string_len])[0]
+        packed_msg = packed_msg[fmt_string_len:]
+        unpacked_msg = struct.unpack(fmt_string, packed_msg)
+        cmd = unpacked_msg[0]
+        values = unpacked_msg[1:]
+    except:
+        raise
     return (cmd, values)
 
 
@@ -130,10 +204,14 @@ def send_img(sock, frame, compressed_transfer=True, level=90):
     to compress, with 0 being fast compression, larger file and 9 longer time, smaller
     file.
 
-    :param sock: the transfer socket
-    :param frame: the image to transfer (OpenCV2 image, i.e. BGR array)
-    :param compressed_transfer: whether to compress during transfer
-    :param level: what level of compression to use, 0-100 (highest) for JPG or 0-9 (smaller file, longer) for PNG
+    Args:
+        sock (socket): the socket to transfer over
+        frame (cv2.image): the image to transfer in OpenCV2 image format, i.e. a BGR array
+        compressed_transfer (bool): whether to compress during transfer
+        level (int):  what level of compression to use, 0-100 (highest) for JPG or 0-9 (smaller file, longer) for PNG
+
+    Returns:
+        None: on success
     """
     image_data = None
     if compressed_transfer:
@@ -156,6 +234,15 @@ def send_img(sock, frame, compressed_transfer=True, level=90):
 
 
 def recv_img(sock):
+    """
+    Receives an image over the given socket and decodes it into an OpenCV2 style image (i.e. a BGR array)
+
+    Args:
+        sock(socket): the socket over which to receive the image
+
+    Returns:
+        [[int, int, int]]: the BGR array holding the OpenCV2 image
+    """
     # Receive image string and decode into an Image
     img_data_str = recv_msg(sock)
     print 'Received image of length: ', len(img_data_str)
