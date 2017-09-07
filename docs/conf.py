c779cef70b1cfedd98d8066a6b694112cd0036b4
Curtis West <curtis@curtiswest.net>
2017-09-07 13:22:38 +1000

    Change docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,10 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+if not on_rtd:
+    html_theme = 'alabaster'
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
